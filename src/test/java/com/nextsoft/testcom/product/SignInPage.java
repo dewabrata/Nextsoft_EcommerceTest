f74c0fb2fb94d6245a0a6f3f7d6c3d1338c3f3e1
Ridho Tadjudin <ridhotadjudin@gmail.com>
2022-06-16 20:36:05 +0700

    team3 update

---
+++ src/test/java/com/nextsoft/testcom/product/SignInPage.java
@@ -0,0 +1,32 @@
+package com.nextsoft.testcom.product;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignInPage {
+	
+	protected WebDriver driver;
+
+	public SignInPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	@FindBy(xpath = "//input[@id='email']")
+	private WebElement email;
+	
+	@FindBy(xpath = "//input[@id='passwd']")
+	private WebElement password;
+	
+	@FindBy(xpath = "//span[normalize-space()='Sign in']")
+	private WebElement btnSignIn;
+	
+	public AccountPage loginValidUser(String username,String usrPassword) {
+		email.sendKeys(username);
+		password.sendKeys(usrPassword);
+		btnSignIn.click();
+		return PageFactory.initElements(driver, AccountPage.class);
+	}
+	
+}
