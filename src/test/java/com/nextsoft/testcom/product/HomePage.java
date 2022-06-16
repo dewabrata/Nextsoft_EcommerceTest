048a9deccd54644e17bfa8406be72238a8e0f4e1
Ridho Tadjudin <ridhotadjudin@gmail.com>
2022-06-16 22:14:11 +0700

    kelompok 2: by ridho

--- src/test/java/com/nextsoft/testcom/product/HomePage.java
+++ src/test/java/com/nextsoft/testcom/product/HomePage.java
@@ -16,6 +16,15 @@ public class HomePage {
 	@FindBy(xpath = "//a[@title='Log in to your customer account']")
 	private WebElement signIn;
 	
+	@FindBy(xpath = "//li[@class='ajax_block_product col-xs-12 col-sm-4 col-md-3 first-in-line first-item-of-tablet-line first-item-of-mobile-line hovered']//span[contains(text(),'Quick view')]")
+	private WebElement btnQuickView;
+	
+	@FindBy(xpath = "//li[@class='ajax_block_product col-xs-12 col-sm-4 col-md-3 first-in-line first-item-of-tablet-line first-item-of-mobile-line hovered']//span[contains(text(),'Quick view')]")
+	private WebElement btnMore;
+
+	@FindBy(xpath = "//li[@class='ajax_block_product col-xs-12 col-sm-4 col-md-3 first-in-line first-item-of-tablet-line first-item-of-mobile-line hovered']//span[contains(text(),'Quick view')]")
+	private WebElement btnAddToCart;
+	
 	public SignInPage clickSignIn() {
 		signIn.click();
 		SignInPage signInPage = PageFactory.initElements(driver, SignInPage.class);
